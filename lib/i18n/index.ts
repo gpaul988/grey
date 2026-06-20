86a4e400531d2f7f85a1a97358cf2b28af7a85ee
Graham Paul <graham@greyinfotech.com>
2026-06-20 22:09:42 +0000

    refactor: Remove all i18n - English-only project

--- lib/i18n/index.ts
+++
@@ -1,3 +0,0 @@
-export { initI18n, i18nConfig } from './config';
-export { getServerI18n, useServerTranslation, getLanguageFromPath, removeLanguagePrefix, addLanguagePrefix } from './server';
-export { useTranslation, useLanguageSwitcher, LanguageSwitcher, useLanguagePreference } from './client';
